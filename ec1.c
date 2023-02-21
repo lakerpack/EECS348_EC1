b82cb17b1118c0f051b0a99a436e1297a4ccec62
lackerpack <thapaanil2007@gmail.com>
2023-02-20 21:26:39 -0600

    Add print methd for arrays

--- ec1.c
+++ ec1.c
@@ -14,9 +14,21 @@ void populateArrays(int programmers[10][10], int departments[10][10]){
 }
 
 
+void printArrays(int programmers[10][10], int departments[10][10]){
+    printf("Departments: \n");
+    for (int i = 0; i < 5; i++){
+        printf("%d %d %d %d %d \n", departments[0][i], departments[1][i], departments[2][i], departments[3][i], departments[4][i]);
+    }
+    printf("Programmers: \n");
+    for (int i = 0; i < 5; i++){
+        printf("%d %d %d %d %d \n", programmers[0][i], programmers[1][i], programmers[2][i], programmers[3][i], programmers[4][i]);
+    }
+}
+
 int main(){
     int programmers[10][10];
     int departments[10][10];
     populateArrays(programmers, departments);
+    printArrays(programmers, departments);
     return 0;
 }
